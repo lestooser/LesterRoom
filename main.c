3a2b62ccc4daf7f1a73044715d7942e22dedafce
lestooser <71587682+lestooser@users.noreply.github.com>
2020-10-03 16:36:41 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -21,30 +21,29 @@ float qd(float a , float b) {
 	return Sum, Dif, Pr, mod;
 }
 void main() {
-	setlocale(LC_ALL, "RUS");
-	printf("Введите стороны прямоугольника : \n");
+	printf("Enter the sides : \n");
 	scanf_s("%f%f", &a, &b);
 	per(a, b);
 	printf("P = %.2f , S = %.2f \n", P, S);
-	printf("Введите диаметр : \n");
+	printf("Enter D : \n");
 	scanf_s("%f", &D);
 	diam(D);
 	printf("L = %.2f\n", res);
-	printf("Введите 2 числа : \n");
+	printf("Enter 2 num : \n");
 	scanf_s("%f%f", &a, &b);
 	res = (a + b) / 2;
-	printf("Среднее арифметическое = %.2f \n ", res);
-	printf("Введите 2 числа : \n");
+	printf("the arithmetic mean = %.2f \n ", res);
+	printf("Enter 2 num : \n");
 	scanf_s("%f%f", &a, &b);
 	a = pow(a, 2);
 	b = pow(b, 2);
 	qd(a, b);
-	printf("Сумма = %.2f, Разность = %.2f, Произведение = %.2f, Частное квадратов = %.2f\n", Sum, Dif, Pr, mod);
+	printf("sum = %.2f, dif = %.2f, prod = %.2f, quo = %.2f\n", Sum, Dif, Pr, mod);
 	printf("Введите последние 2 числа : \n");
 	scanf_s("%f%f", &a, &b);
 	a = fabsf(a);
 	b = fabsf(b);
 	qd(a, b);
-	printf("Сумма = %.2f, Разность = %.2f, Произведение = %.2f, Частное квадратов = %.2f\n", Sum, Dif, Pr, mod);
+	printf("sum = %.2f, dif = %.2f, prod = %.2f, quo = %.2f\n", Sum, Dif, Pr, mod);
 	system("pause")
 }
